50df7086c400eaa2d74b662d845088b6343d05ce
praf002@gmail.com <praf002@gmail.com>
2022-12-16 19:46:43 +0530

    home page test cases

---
+++ src/main/java/PageLayer/HomePage.java
@@ -0,0 +1,11 @@
+package PageLayer;
+
+public class HomePage {
+
+	public void logo()
+	{
+		System.out.println("logo");
+	}
+	
+	
+}
---
+++ src/test/java/Tutorial1/HomePageTest.java
@@ -0,0 +1,16 @@
+package Tutorial1;
+
+import PageLayer.HomePage;
+
+public class HomePageTest {
+
+	public static void main(String[] args) {
+		
+		HomePage homepage =new HomePage();
+		homepage.logo();
+		
+		
+		
+	}
+	
+}
